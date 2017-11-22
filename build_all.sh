3dc61ccee29b3b49dfc83ef2d6bd4519bda0c515
George Pamfilis <gpamfilis@gmail.com>
2017-11-22 17:38:20 +0200

    uploaded to pypi

--- build_all.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-rm -rf dist
-rm -rf build
-
-python setup.py bdist_wheel
-twine upload dist/*
--- setup.py
+++ setup.py
@@ -36,9 +36,9 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.14',
+    version='0.1.15',
 
-    description='A program for counting colonies in petri dishes',
+    description='A program for counting CFUs in petri dishes',
     long_description=long_description,
 
     # The project's main homepage.
